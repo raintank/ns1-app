3ac63c0383b3b42e678f3412f5ca67e7db2a80ba
woodsaj <anthony@monkey.id.au>
2016-04-08 19:27:50 +0800

    update config page to create Task to collector data.

--- dist/config/config.js
+++ dist/config/config.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-System.register(['./config.html!text'], function (_export, _context) {
+System.register(["./config.html!text"], function (_export, _context) {
   var configTemplate, _createClass, Ns1ConfigCtrl;
 
   function _classCallCheck(instance, Constructor) {
@@ -32,21 +32,45 @@ System.register(['./config.html!text'], function (_export, _context) {
         };
       }();
 
-      _export('ConfigCtrl', Ns1ConfigCtrl = function () {
+      _export("ConfigCtrl", Ns1ConfigCtrl = function () {
         function Ns1ConfigCtrl($scope, $injector, backendSrv) {
           _classCallCheck(this, Ns1ConfigCtrl);
 
           this.backendSrv = backendSrv;
-          this.appModel.secureJsonData = {
-            ns1_token: '',
-            gnet_token: ''
-          };
+          this.appModel.secureJsonData = {};
+          if (this.appModel.jsonData === null) {
+            this.appModel.jsonData = {
+              gnetTokenSet: false,
+              ns1TokenSet: false
+            };
+          }
+          this.taskStatus = "Task not found";
+          this.task = {};
           this.error = false;
+          this.appEditCtrl.setPreUpdateHook(this.preUpdate.bind(this));
           this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
+          var self = this;
+          if (this.appModel.enabled) {
+            this.getCustomerId().then(function (resp) {
+              var taskName = self.taskName(resp.customerid);
+              self.getTask(taskName);
+            });
+          }
         }
 
         _createClass(Ns1ConfigCtrl, [{
-          key: 'postUpdate',
+          key: "preUpdate",
+          value: function preUpdate() {
+            if (this.appModel.secureJsonData.ns1_token) {
+              this.appModel.jsonData.ns1TokenSet = true;
+            }
+            if (this.appModel.secureJsonData.gnet_token) {
+              this.appModel.jsonData.gnetTokenSet = true;
+            }
+            return Promise.resolve();
+          }
+        }, {
+          key: "postUpdate",
           value: function postUpdate() {
             var _this = this;
 
@@ -55,8 +79,8 @@ System.register(['./config.html!text'], function (_export, _context) {
               return Promise.resolve();
             }
             // make sure our Api key works.
-            return this.backendSrv.get("api/plugin-proxy/raintank-ns1-app/api/account/settings").then(function (resp) {
-              return ensureTask(resp.customerid).then(function () {
+            return this.getCustomerId().then(function (resp) {
+              return self.ensureTask(resp.customerid).then(function () {
                 return _this.appEditCtrl.importDashboards();
               });
             }, function () {
@@ -65,9 +89,74 @@ System.register(['./config.html!text'], function (_export, _context) {
             });
           }
         }, {
-          key: 'ensureTask',
+          key: "getCustomerId",
+          value: function getCustomerId() {
+            return this.backendSrv.get("api/plugin-proxy/raintank-ns1-app/ns1/account/settings");
+          }
+        }, {
+          key: "taskName",
+          value: function taskName(customerid) {
+            return "NS1-" + customerid;
+          }
+        }, {
+          key: "ensureTask",
           value: function ensureTask(customerid) {
+            var _this2 = this;
+
             var self = this;
+            var taskName = this.taskName(customerid);
+            return this.getTask(taskName).then(function (exists) {
+              if (exists) {
+                self.taskStatus = "Task not created";
+                return;
+              }
+              var task = {
+                "name": taskName,
+                "metrics": { "/raintank/apps/ns1/_all": 0 },
+                "config": {
+                  "/raintank/apps/ns1": {
+                    "ns1_key": self.appModel.secureJsonData.ns1_token
+                  }
+                },
+                "interval": 60,
+                "route": { "type": "any", "config": { id: 1 } },
+                "enabled": true
+              };
+
+              return self.backendSrv.post("/api/plugin-proxy/raintank-ns1-app/tasks", task).then(function (resp) {
+                _this2.task = resp.body;
+                self.taskStatus = "Task running";
+              });
+            });
+          }
+        }, {
+          key: "getTask",
+          value: function getTask(taskName) {
+            var self = this;
+            return this.backendSrv.get("/api/plugin-proxy/raintank-ns1-app/tasks", { metric: "/raintank/apps/ns1/_all", name: taskName }).then(function (resp) {
+              console.log(resp);
+              if (resp.body.length > 0) {
+                self.task = resp.body;
+                self.taskStatus = "Task running";
+                return true;
+              }
+              return false;
+            });
+          }
+        }, {
+          key: "stopTask",
+          value: function stopTask() {
+            var _this3 = this;
+
+            if (!this.task) {
+              console.log("unknown task name.");
+              return;
+            }
+            return this.backendSrv.get("/api/plugin-proxy/raintank-ns1-app/tasks/" + this.task.id).then(function (resp) {
+              console.log(resp);
+              _this3.task = {};
+              _this3.taskStatus = "Task not found";
+            });
           }
         }]);
 
@@ -76,7 +165,7 @@ System.register(['./config.html!text'], function (_export, _context) {
 
       Ns1ConfigCtrl.template = configTemplate;
 
-      _export('ConfigCtrl', Ns1ConfigCtrl);
+      _export("ConfigCtrl", Ns1ConfigCtrl);
     }
   };
 });
--- src/config/config.js
+++ src/config/config.js
@@ -3,12 +3,35 @@ import configTemplate from './config.html!text';
 class Ns1ConfigCtrl {
   constructor($scope, $injector, backendSrv) {
     this.backendSrv = backendSrv;
-    this.appModel.secureJsonData = {
-      ns1_token: '',
-      gnet_token: ''
-    };
+    this.appModel.secureJsonData = {};
+    if (this.appModel.jsonData === null) {
+      this.appModel.jsonData = {
+        gnetTokenSet: false,
+        ns1TokenSet: false
+      };
+    }
+    this.taskStatus = "Task not found";
+    this.task = {};
     this.error = false;
+    this.appEditCtrl.setPreUpdateHook(this.preUpdate.bind(this));
     this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
+    var self = this;
+    if (this.appModel.enabled) {
+      this.getCustomerId().then((resp) => {
+        var taskName = self.taskName(resp.customerid);
+        self.getTask(taskName);
+      });
+    }
+  }
+
+  preUpdate() {
+    if (this.appModel.secureJsonData.ns1_token) {
+      this.appModel.jsonData.ns1TokenSet =true;
+    }
+    if (this.appModel.secureJsonData.gnet_token) {
+      this.appModel.jsonData.gnetTokenSet =true;
+    }
+    return Promise.resolve();
   }
 
   postUpdate() {
@@ -17,9 +40,9 @@ class Ns1ConfigCtrl {
       return Promise.resolve();
     }
     // make sure our Api key works.
-    return this.backendSrv.get("api/plugin-proxy/raintank-ns1-app/api/account/settings")
+    return this.getCustomerId()
     .then((resp) => {
-      return ensureTask(resp.customerid).then(()=> {
+      return self.ensureTask(resp.customerid).then(() => {
         return this.appEditCtrl.importDashboards(); 
       });
     }, () => {
@@ -28,9 +51,65 @@ class Ns1ConfigCtrl {
     })
   }
 
+  getCustomerId() {
+    return this.backendSrv.get("api/plugin-proxy/raintank-ns1-app/ns1/account/settings");
+  }
+
+  taskName(customerid) {
+    return "NS1-" + customerid;
+  }
+
   ensureTask(customerid) {
     var self = this;
-    
+    var taskName = this.taskName(customerid);
+    return this.getTask(taskName).then((exists) => {
+      if (exists) {
+        self.taskStatus = "Task not created";
+        return;
+      }
+      var task = {
+        "name": taskName,
+        "metrics": {"/raintank/apps/ns1/_all":0},
+        "config": {
+          "/raintank/apps/ns1": {
+            "ns1_key": self.appModel.secureJsonData.ns1_token
+          }
+        },
+        "interval": 60,
+        "route": { "type": "any", "config": {id: 1}},
+        "enabled": true
+      };
+
+      return self.backendSrv.post("/api/plugin-proxy/raintank-ns1-app/tasks", task).then((resp) => {
+        this.task = resp.body;
+        self.taskStatus = "Task running";
+      });
+    });
+  }
+
+  getTask(taskName) {
+    var self = this;
+    return this.backendSrv.get("/api/plugin-proxy/raintank-ns1-app/tasks", {metric: "/raintank/apps/ns1/_all", name: taskName}).then((resp) => {
+      console.log(resp);
+      if (resp.body.length > 0 ){
+        self.task = resp.body;
+        self.taskStatus = "Task running";
+        return true;
+      }
+      return false;
+    });
+  }
+
+  stopTask() {
+    if (!this.task) {
+      console.log("unknown task name.");
+      return;
+    }
+    return this.backendSrv.get("/api/plugin-proxy/raintank-ns1-app/tasks/"+this.task.id).then((resp) => {
+      console.log(resp);
+      this.task = {};
+      this.taskStatus = "Task not found";
+    });
   }
 }
 
