fc398fff002c7b7f191894f0002deb500bead733
Matt Toback <matt@raintank.io>
2016-06-14 18:31:27 -0400

    task list pretty much complete

---
+++ dist/components/snaptask/snaptask_list.js
@@ -0,0 +1,27 @@
+'use strict';
+
+System.register([], function (_export, _context) {
+  var SnapTaskListCtrl;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  return {
+    setters: [],
+    execute: function () {
+      _export('SnapTaskListCtrl', SnapTaskListCtrl = function SnapTaskListCtrl(contextSrv) {
+        _classCallCheck(this, SnapTaskListCtrl);
+
+        this.pageReady = true;
+      });
+
+      SnapTaskListCtrl.templateUrl = 'public/plugins/ns1-app/components/snaptask/partials/snaptask_list.html';
+
+      _export('SnapTaskListCtrl', SnapTaskListCtrl);
+    }
+  };
+});
+//# sourceMappingURL=snaptask_list.js.map
---
+++ src/components/snaptask/snaptask_list.js
@@ -0,0 +1,8 @@
+class SnapTaskListCtrl {
+  constructor(contextSrv) {
+    this.pageReady = true;
+  }
+}
+
+SnapTaskListCtrl.templateUrl = 'public/plugins/ns1-app/components/snaptask/partials/snaptask_list.html';
+export {SnapTaskListCtrl};
